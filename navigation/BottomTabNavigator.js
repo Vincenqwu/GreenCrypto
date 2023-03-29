25f9db9926fc2c7e96ec65203745541b3797e9c9
Chen Qiu <qcwssss@outlook.com>
2023-03-29 13:44:16 -0700

    fixed portfolio tab name

--- navigation/BottomTabNavigator.js
+++ navigation/BottomTabNavigator.js
@@ -1,13 +1,13 @@
-import { View, Text, TouchableOpacity } from 'react-native'
-import React from 'react'
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { MaterialCommunityIcons } from '@expo/vector-icons';
-import { MaterialIcons } from '@expo/vector-icons';
+import { View, Text, TouchableOpacity } from "react-native";
+import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { MaterialIcons } from "@expo/vector-icons";
 import { FontAwesome } from "@expo/vector-icons";
-import HomeScreen from '../screens/HomeScreen';
-import ProfileScreen from '../screens/ProfileScreen';
-import PortfolioScreen from '../screens/PortfolioScreen';
-import ActivityListScreen from '../screens/ActivityListScreen';
+import HomeScreen from "../screens/HomeScreen";
+import ProfileScreen from "../screens/ProfileScreen";
+import PortfolioScreen from "../screens/PortfolioScreen";
+import ActivityListScreen from "../screens/ActivityListScreen";
 
 export default function BottomTabNavigator({ navigation }) {
   const Tab = createBottomTabNavigator();
@@ -29,11 +29,7 @@ export default function BottomTabNavigator({ navigation }) {
                 opacity: 0.8,
               }}
             >
-              <MaterialIcons
-                name="add"
-                size={25}
-                color="white"
-              />
+              <MaterialIcons name="add" size={25} color="white" />
             </TouchableOpacity>
           </View>
         ),
@@ -42,7 +38,7 @@ export default function BottomTabNavigator({ navigation }) {
         },
         headerTintColor: "white",
         headerTitleStyle: {
-          fontWeight: 'bold',
+          fontWeight: "bold",
           fontSize: 16,
         },
         tabBarStyle: {
@@ -50,23 +46,22 @@ export default function BottomTabNavigator({ navigation }) {
           paddingHorizontal: 5,
           paddingTop: 0,
           backgroundColor: "blue",
-          position: 'absolute',
+          position: "absolute",
           borderTopWidth: 0,
         },
         tabBarLabelStyle: {
           fontSize: 12,
-          fontWeight: 'bold',
+          fontWeight: "bold",
           marginBottom: 5,
         },
         tabBarActiveTintColor: "orange",
       }}
-  
     >
       <Tab.Screen
         name="Home"
         component={HomeScreen}
         options={{
-          tabBarLabel: 'Home',
+          tabBarLabel: "Home",
           tabBarIcon: ({ color, size }) => (
             <MaterialCommunityIcons name="coffee" color={color} size={size} />
           ),
@@ -76,7 +71,7 @@ export default function BottomTabNavigator({ navigation }) {
         name="Profile"
         component={ProfileScreen}
         options={{
-          tabBarLabel: 'Profile',
+          tabBarLabel: "Profile",
           tabBarIcon: ({ color, size }) => (
             <FontAwesome name="exclamation" color={color} size={size} />
           ),
@@ -86,7 +81,7 @@ export default function BottomTabNavigator({ navigation }) {
         name="Portfolio"
         component={PortfolioScreen}
         options={{
-          tabBarLabel: 'Profile',
+          tabBarLabel: "Portfolio",
           tabBarIcon: ({ color, size }) => (
             <FontAwesome name="exclamation" color={color} size={size} />
           ),
@@ -96,7 +91,7 @@ export default function BottomTabNavigator({ navigation }) {
         name="Activity List"
         component={ActivityListScreen}
         options={{
-          tabBarLabel: 'Activities',
+          tabBarLabel: "Activities",
           tabBarIcon: ({ color, size }) => (
             <FontAwesome name="exclamation" color={color} size={size} />
           ),
