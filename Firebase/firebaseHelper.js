7f5e2034838857547410615ccce188cd3fe1bbc2
Vincenqwu <wuqiuan1997.qw@gmail.com>
2023-03-31 09:34:10 -0700

    update firestore

--- Firebase/firebaseHelper.js
+++ Firebase/firebaseHelper.js
@@ -1,3 +1,23 @@
 import { collection, addDoc, doc, deleteDoc, updateDoc} from "firebase/firestore";
 import { firestore } from "./firebase-setup";
+import { auth } from "./firebase-setup";
 
+export async function createActivity(activity) {
+  try {
+    const docRef = await addDoc(collection(firestore, "activities"), {
+      ...activity,
+      userId: auth.currentUser.uid,
+    });
+    console.log(docRef.id);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+export async function deletePost(postId) {
+  try {
+    await deleteDoc(doc(firestore, "posts", postId));
+  } catch (err) {
+    console.log(err);
+  }
+}
