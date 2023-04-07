4d2f519fe4e87c04364f2330c925266907f19abb
Vincenqwu <wuqiuan1997.qw@gmail.com>
2023-04-07 14:40:33 -0700

    finish search coin feature

--- screens/SearchCoinScreen.js
+++ screens/SearchCoinScreen.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import { View, TextInput, FlatList, Text, StyleSheet } from 'react-native';
+import { View, TextInput, FlatList, Text, StyleSheet, Pressable } from 'react-native';
 import { getAllCryptosData, getMarketData } from '../api/request';
 
-export default function SearchCoinScreen() {
+export default function SearchCoinScreen({ navigation }) {
   const [allCoins, setAllCoins] = useState([]);
   const [searchInput, setSearchInput] = useState('');
   const [searchResults, setSearchResults] = useState([]);
@@ -16,8 +16,9 @@ export default function SearchCoinScreen() {
         const coinId = allCoins[i].id.toLowerCase();
         const coinName = allCoins[i].name.toLowerCase();
         const coinSymbol = allCoins[i].symbol.toLowerCase();
-        if (coinId.includes(lowerInput) || coinName.includes(lowerInput) || coinSymbol.includes(lowerInput)) {
-          results.push(allCoins[i].name + ' (' + allCoins[i].symbol + ')');
+        if (coinId.startsWith(lowerInput) || coinName.startsWith(lowerInput) || coinSymbol.startsWith(lowerInput)) {
+          // results.push(allCoins[i].name + ' (' + allCoins[i].symbol + ')');
+          results.push(allCoins[i]);
         }
       }
       setSearchResults(results);
@@ -27,52 +28,67 @@ export default function SearchCoinScreen() {
     }
   }
 
-  // Retrieeves all coins data from API
+  // Retrieves all coins data from API
   useEffect(() => {
     async function fetchData() {
-      const allCoinsData = await getMarketData();
+      const allCoinsData = await getAllCryptosData();
       setAllCoins(allCoinsData);
     }
     fetchData();
   }, []);
 
+  const handlePress = (coin) => {
+    console.log(coin);
+    navigation.navigate('Details', { id: coin.id, symbol: coin.symbol });
+  }
 
   return (
     <View style={styles.container}>
-      <TextInput
-        style={styles.input}
-        value={searchInput}
-        onChangeText={(text) => {
-          setSearchInput(text);
-          handleSearch(text);
-        }}
-        placeholder="Enter a coin name"
-      />
-      <Text style={styles.resultText}>Search Results:</Text>
+      <View style={styles.searchContainer}>
+        <TextInput
+          style={styles.searchInput}
+          value={searchInput}
+          onChangeText={(text) => {
+            setSearchInput(text);
+            handleSearch(text);
+          }}
+          placeholder="Enter a coin name"
+        />
+      </View>
+      <Text style={styles.resultText}>Results:</Text>
       <FlatList
         data={searchResults}
         renderItem={({ item }) => (
-          <Text style={styles.resultItem}>{item}</Text>
+          <Pressable onPress={() => handlePress(item)} style={styles.resultItem}>
+            <Text>{item.name} ({item.symbol})</Text>
+          </Pressable>
         )}
-        keyExtractor={(item, index) => index.toString()}
+        keyExtractor={(item) => item.id}
+        ListEmptyComponent={() => (
+          <Text style={styles.emptyText}>No results found.</Text>
+        )}
       />
     </View>
   );
 };
 
-const arr = ["Hello", "World", "Vincent", "Vice"];
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 16,
+    backgroundColor: '#F5F5F5',
   },
-  input: {
-    height: 40,
-    backgroundColor: '#f0f0f0',
-    borderRadius: 8,
+  searchContainer: {
+    backgroundColor: '#FFFFFF',
+    borderRadius: 10,
     paddingHorizontal: 10,
+    paddingVertical: 5,
     marginBottom: 16,
+  },
+  searchInput: {
+    height: 40,
+    fontSize: 16,
+    color: '#000000',
   },
   resultText: {
     fontSize: 18,
@@ -82,6 +98,8 @@ const styles = StyleSheet.create({
   resultItem: {
     fontSize: 16,
     marginBottom: 8,
+    backgroundColor: '#FFFFFF',
+    borderRadius: 5,
+    padding: 10,
   },
 });
-
