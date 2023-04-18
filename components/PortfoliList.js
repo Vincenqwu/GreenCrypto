99291cdde5b7921ba0f7e7414f06812f8c1a3bc0
Chen Qiu <qcwssss@outlook.com>
2023-04-18 15:44:23 -0700

    add portfolio screen basic

---
+++ components/PortfoliList.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const PortfoliList = () => {
+  return (
+    <View>
+      <Text>PortfoliList</Text>
+    </View>
+  );
+};
+
+export default PortfoliList;
--- screens/PortfolioScreen.js
+++ screens/PortfolioScreen.js
@@ -1,68 +1,121 @@
-import { View, Text, StyleSheet, Alert } from "react-native";
-import React from "react";
-import { getCryptoData } from "../api/request";
-import { createActivity } from "../Firebase/firebaseHelper";
-import PressableButton from "../components/PressableButton";
-import { Colors } from "../styles/Color";
+import React, { useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import PortfoliList from "../components/PortfoliList";
+import WatchList from "../components/WatchList";
 
-export default function PortfolioScreen() {
-  let testCoinId = "bitcoin";
-  let testAmount = 0.1;
-
-  async function buyCrypto(coinId, amount) {
-    const coinData = await getCryptoData(coinId);
-
-    Alert.alert(
-      "Buy Bitcoin?",
-      `Are you sure you want to buy 0.1 bitcoin at $${coinData.market_data.current_price.usd}?`,
-      [
-        {
-          text: "Cancel",
-          style: "cancel",
-        },
-        {
-          text: "OK",
-          onPress: () => {
-            const newActivity = {
-              action: "buy",
-              coinId: coinId,
-              amount: amount,
-              price: coinData.market_data.current_price.usd,
-              timestamp: coinData.last_updated,
-            };
-            console.log(newActivity);
-            createActivity(newActivity);
-          },
-        },
-      ]
-    );
-  }
+const Portfolio = () => {
+  const [currentBalance, setCurrentBalance] = useState(1000);
+  const [profitLoss, setProfitLoss] = useState(100);
+  const [activeTab, setActiveTab] = useState("portfolio");
 
   return (
-    <View>
-      <PressableButton
-        pressHandler={() => buyCrypto(testCoinId, testAmount)}
-        style={styles.buttonStyle}
-      >
-        <Text style={styles.buttonTextStyle}>Click to Buy Bitcoin</Text>
-      </PressableButton>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <View style={styles.headerItem}>
+          <Text style={styles.headerLabel}>Current Balance:</Text>
+          <Text style={styles.headerValue}>$ {currentBalance}</Text>
+        </View>
+        <View style={styles.headerItem}>
+          <Text style={styles.headerLabel}>Profit/Loss:</Text>
+          <Text
+            style={[
+              styles.headerValue,
+              profitLoss > 0 ? styles.green : styles.red,
+            ]}
+          >
+            {profitLoss > 0 ? "+" : "-"}$ {Math.abs(profitLoss)}
+          </Text>
+        </View>
+      </View>
+      <View style={styles.tabs}>
+        <View
+          style={[
+            styles.tabItem,
+            activeTab === "portfolio" ? styles.activeTab : null,
+          ]}
+        >
+          <Text
+            style={styles.tabLabel}
+            onPress={() => setActiveTab("portfolio")}
+          >
+            Portfolio
+          </Text>
+        </View>
+        <View
+          style={[
+            styles.tabItem,
+            activeTab === "watchlist" ? styles.activeTab : null,
+          ]}
+        >
+          <Text
+            style={styles.tabLabel}
+            onPress={() => setActiveTab("watchlist")}
+          >
+            WatchList
+          </Text>
+        </View>
+      </View>
+      {activeTab === "portfolio" ? <PortfoliList /> : <WatchList />}
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  buttonStyle: {
-    marginTop: 10,
-    backgroundColor: Colors.buttonColor,
-    padding: 10,
-    paddingHorizontal: 20,
-    borderRadius: 5,
-    marginHorizontal: 10,
-    width: 180,
+  container: {
+    flex: 1,
+    padding: 16,
+    backgroundColor: "#fff",
   },
-  buttonTextStyle: {
-    color: "white",
+  header: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginBottom: 16,
+    width: "100%",
+  },
+  headerItem: {
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  headerLabel: {
+    fontSize: 16,
     fontWeight: "bold",
+    color: "#333",
+    textAlign: "center",
   },
-  pressedStyle: { backgroundColor: "red", opacity: 0.2 },
+  headerValue: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "#333",
+    textAlign: "center",
+  },
+  green: {
+    color: "green",
+  },
+  red: {
+    color: "red",
+  },
+  tabs: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    marginBottom: 16,
+  },
+  tabItem: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    paddingVertical: 8,
+    backgroundColor: "#ddd",
+  },
+  tabLabel: {
+    fontSize: 16,
+    fontWeight: "bold",
+    color: "#333",
+    textAlign: "center",
+  },
+  activeTab: {
+    backgroundColor: "#f2f2f2",
+  },
 });
+
+export default Portfolio;
