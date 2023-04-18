562d512c09e31d0b779cccbdb56bd29126bca742
Chen Qiu <qcwssss@outlook.com>
2023-04-18 15:58:45 -0700

    chore: refactor portfolio styles

--- screens/PortfolioScreen.js
+++ screens/PortfolioScreen.js
@@ -2,30 +2,28 @@ import React, { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import PortfoliList from "../components/PortfoliList";
 import WatchList from "../components/WatchList";
+import styles from "../styles/styles";
 
 const Portfolio = () => {
   const [currentBalance, setCurrentBalance] = useState(1000);
-  const [profitLoss, setProfitLoss] = useState(100);
+  const [profit, setProfit] = useState(100);
   const [activeTab, setActiveTab] = useState("portfolio");
 
   return (
-    <View style={styles.container}>
+    <View style={styles.portContainer}>
       <View style={styles.header}>
-        <View style={styles.headerItem}>
-          <Text style={styles.headerLabel}>Current Balance:</Text>
-          <Text style={styles.headerValue}>$ {currentBalance}</Text>
-        </View>
-        <View style={styles.headerItem}>
-          <Text style={styles.headerLabel}>Profit/Loss:</Text>
-          <Text
-            style={[
-              styles.headerValue,
-              profitLoss > 0 ? styles.green : styles.red,
-            ]}
-          >
-            {profitLoss > 0 ? "+" : "-"}$ {Math.abs(profitLoss)}
-          </Text>
-        </View>
+        <Text style={styles.headerLabel}>Current Balance:</Text>
+        <Text style={styles.headerValue}>$ {currentBalance}</Text>
+      </View>
+      <View style={styles.header}>
+        <Text style={styles.headerLabel}>
+          {profit > 0 ? "Profit" : "Loss"}:
+        </Text>
+        <Text
+          style={[styles.headerValue, profit > 0 ? styles.green : styles.red]}
+        >
+          {profit > 0 ? "+" : "-"}$ {Math.abs(profit)}
+        </Text>
       </View>
       <View style={styles.tabs}>
         <View
@@ -59,63 +57,5 @@ const Portfolio = () => {
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 16,
-    backgroundColor: "#fff",
-  },
-  header: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    marginBottom: 16,
-    width: "100%",
-  },
-  headerItem: {
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  headerLabel: {
-    fontSize: 16,
-    fontWeight: "bold",
-    color: "#333",
-    textAlign: "center",
-  },
-  headerValue: {
-    fontSize: 20,
-    fontWeight: "bold",
-    color: "#333",
-    textAlign: "center",
-  },
-  green: {
-    color: "green",
-  },
-  red: {
-    color: "red",
-  },
-  tabs: {
-    flexDirection: "row",
-    justifyContent: "center",
-    alignItems: "center",
-    marginBottom: 16,
-  },
-  tabItem: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    paddingVertical: 8,
-    backgroundColor: "#ddd",
-  },
-  tabLabel: {
-    fontSize: 16,
-    fontWeight: "bold",
-    color: "#333",
-    textAlign: "center",
-  },
-  activeTab: {
-    backgroundColor: "#f2f2f2",
-  },
-});
 
 export default Portfolio;
--- styles/styles.js
+++ styles/styles.js
@@ -1,11 +1,45 @@
 import { StyleSheet } from "react-native";
 
+const basicContainer = {
+  flex: 1,
+  backgroundColor: "#fff",
+};
+
 const styles = StyleSheet.create({
   container: {
-    // backgroundColor: "red",
     flex: 1,
     backgroundColor: "#fff",
     justifyContent: "center",
+  },
+  portContainer: {
+    ...basicContainer,
+    backgroundColor: "#fff",
+    padding: 16,
+  },
+  header: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingLeft: 20,
+    marginBottom: 20,
+    width: "90%",
+    alignItems: "center",
+    // justifyContent: "center",
+  },
+  headerItem: {
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  headerLabel: {
+    fontSize: 22,
+    fontWeight: "bold",
+    color: "#333",
+    textAlign: "center",
+  },
+  headerValue: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "#333",
+    textAlign: "center",
   },
   title: {
     color: "white",
@@ -32,6 +66,34 @@ const styles = StyleSheet.create({
     paddingHorizontal: 5,
     borderRadius: 5,
     marginRight: 5,
+  },
+  green: {
+    color: "green",
+  },
+  red: {
+    color: "red",
+  },
+  tabs: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    marginBottom: 16,
+  },
+  tabItem: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    paddingVertical: 8,
+    backgroundColor: "#ddd",
+  },
+  tabLabel: {
+    fontSize: 16,
+    fontWeight: "bold",
+    color: "#333",
+    textAlign: "center",
+  },
+  activeTab: {
+    backgroundColor: "#f2f2f2",
   },
 });
 
