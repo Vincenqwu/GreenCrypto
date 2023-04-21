9e66f1f1c120822930266869bf81c89130ecaa99
Chen Qiu <qcwssss@outlook.com>
2023-04-20 21:03:07 -0700

    feat: watchlist works

--- components/WatchList.js
+++ components/WatchList.js
@@ -1,13 +1,46 @@
 import { View, Text } from "react-native";
-import React from "react";
-import coins from "../assets/data/cryptos-dup.json";
+import React, { useEffect } from "react";
 import CoinsList from "./CoinsList";
+import { onSnapshot, collection, query, where } from "firebase/firestore";
+import { auth, firestore } from "../Firebase/firebase-setup";
+import { getCryptoDetailsBasedOnIds } from "../api/request";
 
-const WatchList = () => {
+const WatchList = ({ watchlist, setWatchlist }) => {
+  const currentUser = auth.currentUser;
+  useEffect(() => {
+    const unsubscribeWatchlist = onSnapshot(
+      query(
+        collection(firestore, "profiles"),
+        where("uid", "==", currentUser?.uid)
+      ),
+      async (querySnapshot) => {
+        if (!querySnapshot.empty) {
+          let snap = querySnapshot.docs.at(0);
+          let currentWatchlist = snap.data().watchList;
+
+          if (currentWatchlist.length === 0) {
+            setWatchlist([]);
+            return;
+          }
+          getCryptoDetailsBasedOnIds(currentWatchlist).then((res) => {
+            setWatchlist(res);
+          });
+        }
+      },
+      (error) => {
+        console.log("watchlist onsnapshot error: ", error);
+      }
+    );
+    return () => {
+      if (!currentUser.uid) return;
+      unsubscribeWatchlist();
+    };
+  }, [currentUser]);
+
   return (
     <View>
       <Text>WatchList</Text>
-      <CoinsList coins={coins} />
+      <CoinsList coins={watchlist} />
     </View>
   );
 };
